c02b480c6b87e8419ede0485504d9d34b9036665
boc4life <lheandrew@gmail.com>
2019-02-20 17:48:09 -0500

    preloaded friends data, disabled submit button on successful submission to attempt preventing double posts

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,6 +3,16 @@ let friends = [
         name: "Andrew",
         photo: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3059318.png&w=350&h=254",
         scores: [4, 5, 5, 3, 5, 3, 1, 5, 5, 4]
+    },
+    {
+        name: "Ben",
+        photo: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3907387.png&w=350&h=254",
+        scores: [1, 2, 5, 2, 1, 2, 5, 3, 1, 4]
+    },
+    {
+        name: "Josie",
+        photo: "https://www.wwwallaboutcats.com/wp-content/uploads/2016/10/shutterstock_454564069.jpg",
+        scores: [1, 2, 2, 5, 2, 5, 2, 1, 5, 2]
     }
 ];
 
